ffe7349605174b42f97125870b7c3608e44f2c6b
Sachin Boban <sachinboban@users.noreply.github.com>
2017-09-23 14:14:37 +0530

    Added hexdump.py
    Basic version of hexdump script
    
    Signed-off-by: Sachin Boban <sachinboban@users.noreply.github.com>

---
+++ hexdump.py
@@ -0,0 +1,40 @@
+import sys
+import struct
+import os.path
+
+LINE_WIDTH = 16
+InputBinFile = ''
+
+class BinParser:
+    def __init__(self, binFile):
+        self.binaryFile = binFile
+        self.binaryFileSz = os.path.getsize(binFile)
+        self.addrOffset = 0
+    def HexDump(self):
+        binHandle = open(self.binaryFile, 'rb')
+        byteCnt = 0
+        LineBuffer = ''
+        while byteCnt < self.binaryFileSz:
+            if byteCnt % LINE_WIDTH == 0:
+                LineBuffer += '{0:08x}:'.format((byteCnt + self.addrOffset))
+            if byteCnt % 2 == 0:
+                LineBuffer += ' '
+            byte = binHandle.read(1)
+            val = struct.unpack('B', byte)[0]
+            LineBuffer += '{0:02x}'.format(val)
+            if byteCnt % LINE_WIDTH == LINE_WIDTH - 1:
+                LineBuffer += '\n'
+            byteCnt += 1
+        print LineBuffer
+def Usage():
+    print """python hexdump.py binfile"""
+def ValidateCmdLineArgs():
+    if not os.path.isfile(sys.argv[1]):
+        print 'File {0} does not exist'.format(InputBinFile)
+        Usage()
+        sys.exit(1)
+
+#******************************************************************************
+ValidateCmdLineArgs()
+binParser = BinParser(sys.argv[1])
+binParser.HexDump()
