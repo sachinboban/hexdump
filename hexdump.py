c7d7aad1e60c13db84d91f86ddfa9ac281a62f5a
Sachin Boban <sachinboban@users.noreply.github.com>
2017-09-23 14:14:37 +0530

    Added printable ASCII characters
    Added support to display printable ASCII characters
    as those ASCII characters and all others as a
    period('.').
    
    Signed-off-by: Sachin Boban <sachinboban@users.noreply.github.com>

--- hexdump.py
+++ hexdump.py
@@ -14,16 +14,24 @@ class BinParser:
         binHandle = open(self.binaryFile, 'rb')
         byteCnt = 0
         LineBuffer = ''
+        charBuffer = ''
         while byteCnt < self.binaryFileSz:
             if byteCnt % LINE_WIDTH == 0:
+                charBuffer = ''
                 LineBuffer += '{0:08x}:'.format((byteCnt + self.addrOffset))
             if byteCnt % 2 == 0:
                 LineBuffer += ' '
             byte = binHandle.read(1)
             val = struct.unpack('B', byte)[0]
+            if val < 32 or val > 126:
+                charBuffer += '.'
+            else:
+                charBuffer += chr(val)
             LineBuffer += '{0:02x}'.format(val)
             if byteCnt % LINE_WIDTH == LINE_WIDTH - 1:
-                LineBuffer += '\n'
+                LineBuffer += '  |'
+                LineBuffer += charBuffer
+                LineBuffer += '|\n'
             byteCnt += 1
         print LineBuffer
 def Usage():
